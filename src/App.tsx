ccb3b82f01ebe4188c200944a853d7ced71f5ba2
Bong <borobong230@gmail.com>
2023-06-02 07:27:23 +0900

    initialize openai tutorial

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+import "./App.css";
+
+function App() {
+  // const configuration = new Configuration({
+  //   apiKey: import.meta.env.VITE_OPENAI_KEY,
+  // });
+  console.log("test message");
+
+  // const openai = new OpenAIApi(configuration);
+  // const generateImage = async () => {
+  //   const response = await openai.createImage({
+  //     prompt: "Say this is a test",
+  //     n: 1,
+  //     size: "1024x1024",
+  //   });
+  //   // console.log("response", response.data.["data"][0]["url"]);
+  // };
+  return <div></div>;
+}
+
+export default App;
