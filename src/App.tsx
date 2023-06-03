8634dde47913106245f660c35a821376f579d06d
Bong <borobong230@gmail.com>
2023-06-03 22:35:50 +0900

    feat: OpenAI Tutorial Creating an image generator (add image title input, add create image button, add image window)

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,45 @@ import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
-  // const configuration = new Configuration({
-  //   apiKey: import.meta.env.VITE_OPENAI_KEY,
-  // });
-  console.log("test message");
+  const [prompt, setPrompt] = useState("");
+  const [result, setResult] = useState<any>("");
+  const configuration = new Configuration({
+    apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+  });
 
-  // const openai = new OpenAIApi(configuration);
-  // const generateImage = async () => {
-  //   const response = await openai.createImage({
-  //     prompt: "Say this is a test",
-  //     n: 1,
-  //     size: "1024x1024",
-  //   });
-  //   // console.log("response", response.data.["data"][0]["url"]);
-  // };
-  return <div></div>;
+  const openai = new OpenAIApi(configuration);
+
+  const generateImage = async () => {
+    try {
+      console.log("clicked");
+
+      const response = await openai.createImage({
+        prompt: prompt,
+        n: 1,
+        size: "512x512",
+      });
+      setResult(response?.data?.data[0]?.url);
+    } catch (e) {
+      console.log("something is wrong", e);
+    }
+  };
+
+  return (
+    <div className="app-main">
+      <h3>Generate an Image using Open AI API</h3>
+      <input
+        className="app-input"
+        placeholder="Type something to Generate an Image.."
+        onChange={e => setPrompt(e.target.value)}
+      />
+      <button onClick={generateImage}>Generate an Image</button>
+      {result.length > 0 ? (
+        <img className="result-image" src={result} alt="result" />
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default App;
